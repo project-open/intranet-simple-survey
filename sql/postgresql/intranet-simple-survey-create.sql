8c058757968556834de7c40ea80da3ba36acf259
Frank Bergmann <frank.bergmann@project-open.com>
2015-09-23 11:36:06 +0000

    - Updated SQL files

--- sql/postgresql/intranet-simple-survey-create.sql
+++ sql/postgresql/intranet-simple-survey-create.sql
@@ -301,7 +301,7 @@ declare
 begin
 	v_plugin := im_component_plugin__new (
 		null,					-- plugin_id
-		'acs_object',				-- object_type
+		'im_component_plugin',				-- object_type
 		now(),					-- creation_date
 		null,					-- creation_user
 		null,					-- creation_ip
